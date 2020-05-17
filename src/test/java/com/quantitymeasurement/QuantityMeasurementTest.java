c0ce3ae32bed8ec8a27908437c914374ea45cd30
PalakPartani <partanipalak16@gmail.com>
2020-05-17 01:38:25 -0700

    ADDED:feature to check equality for 1 inch & 1 feet

--- src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/quantitymeasurement/QuantityMeasurementTest.java
@@ -90,4 +90,11 @@ public class QuantityMeasurementTest {
         double value2 = quantityMeasurement.getConvertedUnit(1.0,Units.INCH);
         Assert.assertNotEquals(value1, value2, 0.0);
     }
+    @Test
+    public void givenOneInchAndOneFeet_WhenNotEqual_ShouldReturnTrue() {
+        double value1 = quantityMeasurement.getConvertedUnit(1.0,Units.INCH);
+        double value2 = quantityMeasurement.getConvertedUnit(1.0,Units.FEET);
+        Assert.assertNotEquals(value1, value2, 0.0);
+    }
+
 }
